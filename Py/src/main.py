d6d419605c0bbaa6fa50a9e8bc3cc4f5abf8f141
snxx-lppxx <snmgreenx@gmail.com>
2020-12-19 22:18:09 +0500

    Use async

--- Py/src/main.py
+++ Py/src/main.py
@@ -1,4 +1,26 @@
 import discord
+from discord.ext import commands
 
-class Hero(discord.Client):
-	pass 
+bot = commands.Bot(command_prefix='\\?')
+
+@bot.command()
+async def Jobs(ctx):
+	await ctx.send("Commands: \n\n1. '.me'\n2. '.xp'")
+
+class main(discord.Client):
+	async def on_reading(self):
+		print("", self.user)
+
+	async def on_messages(self, message):
+		if message.author != self.user: 
+			print("Hey, I don't understand")
+
+		# don't send this code 	
+		if	message.author == self.user:
+			return
+
+		if message.content == ".help":
+			await message.channel.send("Hi!")
+
+client = main()
+client.run('token')
